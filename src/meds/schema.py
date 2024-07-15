52d560f2b03c48493d2a2cddc64b190534f2cdc0
Ethan Steinberg <ethan.steinberg@gmail.com>
2024-07-15 06:19:32 -0700

    Update schema.py

--- src/meds/schema.py
+++ src/meds/schema.py
@@ -4,24 +4,42 @@ from typing import Any, List, Mapping, Optional
 import pyarrow as pa
 from typing_extensions import NotRequired, TypedDict
 
-# Medical Event Data Standard consists of three main components:
-# 1. A patient data schema
+# Medical Event Data Standard consists of four main components:
+# 1. A patient event schema
 # 2. A label schema
 # 3. A dataset metadata schema.
+# 4. A code metadata schema.
 #
-# Patient data and labels are specified using pyarrow. Dataset metadata is specified using JSON.
+# Event data, labels, and code metadata is specified using pyarrow. Dataset metadata is specified using JSON.
 
-# We also provide TypedDict Python type signatures for these schemas.
+# We also specify a directory structure for how these should be laid out on disk.
+
+# Every MEDS extract consists of a folder that contains both metadata and patient data with the following structure:
+# - data/
+#    A (possibly) nested folder containing multiple parquet files containing patient event data following the events_schema folder.
+#    glob("data/**/*.parquet") is the recommended way for obtaining all patient event files.
+# - dataset_metadata.json
+#    Dataset level metadata containing information about the ETL used, data version, etc
+# - code_metadata.parquet
+#    Code level metadata containing information about the code descriptions, standard mappings, etc
 
 ############################################################
 
-# The patient data schema.
+# The patient event data schema.
+#
+# Patient event data also must satisfy two important properties:
+#
+# 1. Patient event data cannot be split across parquet files. If a patient is in a dataset it must be in one and only one parquet file.
+# 2. Patient event data must be contiguous within a particular parquet file and sorted by event time. 
+
+# Both of these restrictions allow the stream rolling processing (see https://docs.pola.rs/api/python/stable/reference/dataframe/api/polars.DataFrame.rolling.html),
+# which vastly simplifies many data analysis pipelines.
 
 # We define some codes for particularly important events
 birth_code = "SNOMED/184099003"
 death_code = "SNOMED/419620001"
 
-def events_schema(patient_id_type, custom_per_event_properties=[]):
+def patient_events_schema(custom_per_event_properties=[]):
     return pa.schema(
         [
             ("patient_id", pa.int64()),   
@@ -30,6 +48,9 @@ def events_schema(patient_id_type, custom_per_event_properties=[]):
             ("numeric_value", pa.float32()),
         ] + custom_per_event_properties
     )
+
+# No python type is provided because Python tools for processing MEDS data will often provide their own types.
+# See https://github.com/EthanSteinberg/meds_reader/blob/0.0.6/src/meds_reader/__init__.pyi#L55 for example.
 
 ############################################################
 
@@ -55,26 +76,14 @@ Label = TypedDict("Label", {
     "integer_value" : Optional[int],
     "float_value" : Optional[float],
     "categorical_value" : Optional[str],
-})
+}, total=False)
 
 ############################################################
 
 # The dataset metadata schema.
 # This is a JSON schema.
-# This data should be stored in metadata.json within the dataset folder.
+# This data should be stored in dataset_metadata.json within the dataset folder.
 
-code_metadata_entry = {
-    "type": "object",
-    "properties": {
-        "description": {"type": "string"},
-        "parent_codes": {"type": "array", "items": {"type": "string"}},
-    },
-}
-
-code_metadata = {
-    "type": "object",
-    "additionalProperties": code_metadata_entry,
-}
 
 dataset_metadata = {
     "type": "object",
@@ -83,15 +92,12 @@ dataset_metadata = {
         "dataset_version": {"type": "string"},
         "etl_name": {"type": "string"},
         "etl_version": {"type": "string"},
-        "code_metadata": code_metadata,
         "meds_version": {"type": "string"},
     },
 }
 
-# Python types for the above schema
+# Python type for the above schema
 
-CodeMetadataEntry = TypedDict("CodeMetadataEntry", {"description": str, "parent_codes": List[str]})
-CodeMetadata = Mapping[str, CodeMetadataEntry]
 DatasetMetadata = TypedDict(
     "DatasetMetadata",
     {
@@ -99,7 +105,28 @@ DatasetMetadata = TypedDict(
         "dataset_version": NotRequired[str],
         "etl_name": NotRequired[str],
         "etl_version": NotRequired[str],
-        "code_metadata": NotRequired[CodeMetadata],
         "meds_version": NotRequired[str],
     },
+    total=False,
 )
+
+############################################################
+
+# The code metadata schema.
+# This is a parquet schema.
+# This data should be stored in code_metadata.parquet within the dataset folder.
+
+def code_metadata_schema(custom_per_event_properties=[]): 
+    code_metadata = pa.schema(
+        [
+            ("code", pa.string()),
+            ("description", pa.string()),
+            ("parent_codes", pa.list(pa.string()),
+        ] + custom_per_event_properties
+    )
+
+    return code_metadata
+
+# Python type for the above schema
+
+CodeMetadata = TypedDict("CodeMetadata", {"code": str, "description": str, "parent_codes": List[str]}, total=False)
