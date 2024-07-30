dd4ca79c7ba168adc1f9c0e2948db9e203986adb
Matthew McDermott <mattmcdermott8@gmail.com>
2024-07-30 14:41:16 -0400

    Adding missing close paren

--- src/meds/schema.py
+++ src/meds/schema.py
@@ -131,7 +131,7 @@ def code_metadata_schema(custom_per_code_properties=[]):
         [
             ("code", pa.string()),
             ("description", pa.string()),
-            ("parent_codes", pa.list(pa.string()),
+            ("parent_codes", pa.list(pa.string())),
         ] + custom_per_code_properties
     )
 
